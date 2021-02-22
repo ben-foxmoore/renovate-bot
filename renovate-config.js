8c1f83d532ba6d6a0d18a692163694b8d0c2df5b
Ben Fox-Moore <ben.foxmoore@accelleran.com>
2021-02-22 16:57:39 +0100

    Use configuration recommended by jdbruijn
    * Create renovate-config.js
    
    * Switch workflow to use js file
    
    * Update renovatebot/github-action to v24.57.0

---
+++ renovate-config.js
@@ -0,0 +1,16 @@
+const branchName = 'github-renovate';
+
+module.exports = {
+    branchPrefix: `${branchName}/`,
+    dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    logLevel: 'debug',
+    onboarding: true,
+    onboardingBranch: `${branchName}/configure`,
+    platform: 'github',
+    dryRun: false,
+    username: 'ben-foxmoore',
+    repositories: [
+        'ben-foxmoore/renovate-bot',
+    ],
+};
