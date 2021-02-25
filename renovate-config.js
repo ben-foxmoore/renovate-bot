01bb8437025a46f20674c73f9dbd71f5e0551e05
Ben Fox-Moore <ben.foxmoore@accelleran.com>
2021-02-25 20:41:39 +0100

    Remove hard-coded LOG_LEVEL=debug

--- renovate-config.js
+++ renovate-config.js
@@ -4,7 +4,6 @@ module.exports = {
     branchPrefix: `${branchName}/`,
     dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    logLevel: 'debug',
     onboarding: true,
     onboardingBranch: `${branchName}/configure`,
     platform: 'github',
