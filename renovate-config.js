ec6353ac3e3962c31da8ff4c9559f5b952eccad4
Ben Fox-Moore <ben.foxmoore@accelleran.com>
2021-02-25 22:24:55 +0100

    Revert "Remove hard-coded LOG_LEVEL=debug"
    This reverts commit 01bb8437025a46f20674c73f9dbd71f5e0551e05.

--- renovate-config.js
+++ renovate-config.js
@@ -4,6 +4,7 @@ module.exports = {
     branchPrefix: `${branchName}/`,
     dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    logLevel: 'debug',
     onboarding: true,
     onboardingBranch: `${branchName}/configure`,
     platform: 'github',
