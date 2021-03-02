7ae79911f06604d1da66f8605dfddd184e52d231
Ben Fox-Moore <ben.foxmoore@accelleran.com>
2021-03-02 12:03:37 +0100

    Revert "Set Log Level to 'trace'"
    This reverts commit c0133a89a02a34eae50badcf3ad316e86ca8ddda.

--- renovate-config.js
+++ renovate-config.js
@@ -4,7 +4,7 @@ module.exports = {
     branchPrefix: `${branchName}/`,
     dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    logLevel: 'trace',
+    logLevel: 'debug',
     onboarding: true,
     onboardingBranch: `${branchName}/configure`,
     platform: 'github',
