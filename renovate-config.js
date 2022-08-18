b2ac7725467918afabfcf44ff3b04aa8af044b33
Ben Fox-Moore <ben.foxmoore@accelleran.com>
2022-08-18 21:52:27 +0200

    Update configuration to support Renovate v32

--- renovate-config.js
+++ renovate-config.js
@@ -4,11 +4,9 @@ module.exports = {
     branchPrefix: `${branchName}/`,
     dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    logLevel: 'debug',
-    onboarding: true,
     onboardingBranch: `${branchName}/configure`,
     platform: 'github',
-    dryRun: false,
+    dryRun: null,
     username: 'self-renovate[bot]',
     repositories: [
         'ben-foxmoore/renovate-bot',
